b5d8068a77bddaaf339d1accb1c2c1d41d9db1ab
Mingqian 'sunarium' Yu <m.yu@student.unsw.edu.au>
2019-10-15 14:56:26 +1100

    WIP:engine

---
+++ src/config.py
@@ -0,0 +1,25 @@
+DECK_SIZE = 40
+
+# costs of deploying each pieces
+MANA_COST = {
+    'pawn' :2,
+    'rook' :10,
+    'queen':20
+}
+
+# mana avaliable to player initially
+INIT_MANA = 1
+
+# mana gained each turn, set to 0 to turn off
+MANA_PER_TURN = 2
+
+# mana gained for placing this piece on mana pile
+MANA_GAIN = {
+    'pawn':1,
+    'rook':5,
+    'queen':10
+}
+
+# size of board
+BOARD_SIZE = 8
+
